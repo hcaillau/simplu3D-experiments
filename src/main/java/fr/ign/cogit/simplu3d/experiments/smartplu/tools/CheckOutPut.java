3e9b084ff178f25d8bef5c181383e0e4970cad97
Mickaël Brasebin <mickael.brasebin@ign.fr>
2018-10-22 13:44:37 +0200

    Adding mkdirs to checkoutput method

--- src/main/java/fr/ign/cogit/simplu3d/experiments/smartplu/tools/CheckOutPut.java
+++ src/main/java/fr/ign/cogit/simplu3d/experiments/smartplu/tools/CheckOutPut.java
@@ -17,13 +17,15 @@ public class CheckOutPut {
 	public static void main(String[] args) throws Exception {
 
 		// Folder with the output of the results
-		String folderResults = "/home/mbrasebin/.openmole/ZBOOK-SIGOPT-2016/webui/projects/smartPLU/results/22278/";
+		String folderResults = "/home/mbrasebin/.openmole/ZBOOK-SIGOPT-2016/webui/projects/smartPLU/results/61230_first/";
 
 		// Folder with the data used for the simulation
-		String folderDataIn = "/home/mbrasebin/.openmole/ZBOOK-SIGOPT-2016/webui/projects/smartPLU/dataBasicSimu/22278/";
+		String folderDataIn = "/home/mbrasebin/.openmole/ZBOOK-SIGOPT-2016/webui/projects/smartPLU/dataBasicSimu/61230_temp/";
 
 		// Folder with the data that were not simulated
-		String folderOut = "/home/mbrasebin/.openmole/ZBOOK-SIGOPT-2016/webui/projects/smartPLU/dataBasicSimu/22278-temp/";
+		String folderOut = "/home/mbrasebin/.openmole/ZBOOK-SIGOPT-2016/webui/projects/smartPLU/dataBasicSimu/61230/";
+		
+		(new File(folderOut)).mkdirs();
 
 		for (File f : (new File(folderDataIn)).listFiles()) {
 
